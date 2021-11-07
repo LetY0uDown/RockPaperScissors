400d64fb13183e94826f70d75c291cb98faffb34
Maksim <maks.manin.04@gmail.com>
2021-11-07 17:45:39 +1000

    Улучшения в дизайне и коде приложения

--- RockPaperScissors/Form1.Designer.cs
+++ RockPaperScissors/Form1.Designer.cs
@@ -29,52 +29,54 @@ namespace RockPaperScissors
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.SplitContainer splitContainer1;
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.labelGameResult = new System.Windows.Forms.Label();
             this.labelCompOutcome = new System.Windows.Forms.Label();
             this.labelStats = new System.Windows.Forms.Label();
             this.bScissors = new System.Windows.Forms.Button();
             this.bPaper = new System.Windows.Forms.Button();
             this.bRock = new System.Windows.Forms.Button();
-            splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).BeginInit();
-            splitContainer1.Panel1.SuspendLayout();
-            splitContainer1.Panel2.SuspendLayout();
-            splitContainer1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bExit = new FontAwesome.Sharp.IconButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
-            splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            splitContainer1.IsSplitterFixed = true;
-            splitContainer1.Location = new System.Drawing.Point(0, 0);
-            splitContainer1.Name = "splitContainer1";
-            splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
-            splitContainer1.Panel1.Controls.Add(this.labelGameResult);
-            splitContainer1.Panel1.Controls.Add(this.labelCompOutcome);
-            splitContainer1.Panel1.Controls.Add(this.labelStats);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            this.splitContainer1.Panel1.Controls.Add(this.labelGameResult);
+            this.splitContainer1.Panel1.Controls.Add(this.labelCompOutcome);
             // 
             // splitContainer1.Panel2
             // 
-            splitContainer1.Panel2.Controls.Add(this.bScissors);
-            splitContainer1.Panel2.Controls.Add(this.bPaper);
-            splitContainer1.Panel2.Controls.Add(this.bRock);
-            splitContainer1.Size = new System.Drawing.Size(259, 207);
-            splitContainer1.SplitterDistance = 147;
-            splitContainer1.TabIndex = 0;
+            this.splitContainer1.Panel2.Controls.Add(this.bScissors);
+            this.splitContainer1.Panel2.Controls.Add(this.bPaper);
+            this.splitContainer1.Panel2.Controls.Add(this.bRock);
+            this.splitContainer1.Size = new System.Drawing.Size(259, 266);
+            this.splitContainer1.SplitterDistance = 203;
+            this.splitContainer1.TabIndex = 0;
             // 
             // labelGameResult
             // 
             this.labelGameResult.AutoSize = true;
-            this.labelGameResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelGameResult.Location = new System.Drawing.Point(38, 104);
+            this.labelGameResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold);
+            this.labelGameResult.Location = new System.Drawing.Point(38, 139);
             this.labelGameResult.Name = "labelGameResult";
-            this.labelGameResult.Size = new System.Drawing.Size(182, 25);
+            this.labelGameResult.Size = new System.Drawing.Size(209, 29);
             this.labelGameResult.TabIndex = 1;
             this.labelGameResult.Text = "Результат игры";
             // 
@@ -82,7 +84,7 @@ namespace RockPaperScissors
             // 
             this.labelCompOutcome.AutoSize = true;
             this.labelCompOutcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.labelCompOutcome.Location = new System.Drawing.Point(39, 45);
+            this.labelCompOutcome.Location = new System.Drawing.Point(39, 65);
             this.labelCompOutcome.Name = "labelCompOutcome";
             this.labelCompOutcome.Size = new System.Drawing.Size(160, 20);
             this.labelCompOutcome.TabIndex = 1;
@@ -91,18 +93,23 @@ namespace RockPaperScissors
             // labelStats
             // 
             this.labelStats.AutoSize = true;
-            this.labelStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold);
-            this.labelStats.Location = new System.Drawing.Point(3, 0);
+            this.labelStats.BackColor = System.Drawing.Color.Transparent;
+            this.labelStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.labelStats.ForeColor = System.Drawing.Color.LightGray;
+            this.labelStats.Location = new System.Drawing.Point(3, 4);
             this.labelStats.Name = "labelStats";
-            this.labelStats.Size = new System.Drawing.Size(229, 15);
+            this.labelStats.Size = new System.Drawing.Size(206, 13);
             this.labelStats.TabIndex = 0;
             this.labelStats.Text = "Побед: 0 Поражений: 0 Ничьих: 0";
             // 
             // bScissors
             // 
-            this.bScissors.Location = new System.Drawing.Point(91, 7);
+            this.bScissors.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bScissors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bScissors.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bScissors.Location = new System.Drawing.Point(170, 0);
             this.bScissors.Name = "bScissors";
-            this.bScissors.Size = new System.Drawing.Size(75, 40);
+            this.bScissors.Size = new System.Drawing.Size(85, 55);
             this.bScissors.TabIndex = 1;
             this.bScissors.Text = "Ножницы";
             this.bScissors.UseVisualStyleBackColor = true;
@@ -110,9 +117,12 @@ namespace RockPaperScissors
             // 
             // bPaper
             // 
-            this.bPaper.Location = new System.Drawing.Point(170, 7);
+            this.bPaper.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bPaper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bPaper.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bPaper.Location = new System.Drawing.Point(85, 0);
             this.bPaper.Name = "bPaper";
-            this.bPaper.Size = new System.Drawing.Size(75, 40);
+            this.bPaper.Size = new System.Drawing.Size(85, 55);
             this.bPaper.TabIndex = 2;
             this.bPaper.Text = "Бумага";
             this.bPaper.UseVisualStyleBackColor = true;
@@ -120,33 +130,67 @@ namespace RockPaperScissors
             // 
             // bRock
             // 
-            this.bRock.Location = new System.Drawing.Point(10, 7);
+            this.bRock.Dock = System.Windows.Forms.DockStyle.Left;
+            this.bRock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bRock.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.bRock.Location = new System.Drawing.Point(0, 0);
             this.bRock.Name = "bRock";
-            this.bRock.Size = new System.Drawing.Size(75, 40);
+            this.bRock.Size = new System.Drawing.Size(85, 55);
             this.bRock.TabIndex = 0;
             this.bRock.Text = "Камень";
             this.bRock.UseVisualStyleBackColor = true;
             this.bRock.Click += new System.EventHandler(this.bRock_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.panel1.Controls.Add(this.bExit);
+            this.panel1.Controls.Add(this.labelStats);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(255, 21);
+            this.panel1.TabIndex = 2;
+            // 
+            // bExit
+            // 
+            this.bExit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bExit.FlatAppearance.BorderSize = 0;
+            this.bExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.bExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.bExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bExit.IconChar = FontAwesome.Sharp.IconChar.Times;
+            this.bExit.IconColor = System.Drawing.Color.Red;
+            this.bExit.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.bExit.IconSize = 21;
+            this.bExit.Location = new System.Drawing.Point(234, 0);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(21, 21);
+            this.bExit.TabIndex = 0;
+            this.bExit.UseVisualStyleBackColor = true;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
-            this.ClientSize = new System.Drawing.Size(259, 207);
-            this.Controls.Add(splitContainer1);
+            this.ClientSize = new System.Drawing.Size(259, 266);
+            this.ControlBox = false;
+            this.Controls.Add(this.splitContainer1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Камень Ножницы Бумага";
-            splitContainer1.Panel1.ResumeLayout(false);
-            splitContainer1.Panel1.PerformLayout();
-            splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).EndInit();
-            splitContainer1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -159,6 +203,9 @@ namespace RockPaperScissors
         private System.Windows.Forms.Label labelStats;
         private System.Windows.Forms.Label labelCompOutcome;
         private System.Windows.Forms.Label labelGameResult;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton bExit;
     }
 }
 
--- RockPaperScissors/Form1.cs
+++ RockPaperScissors/Form1.cs
@@ -6,61 +6,68 @@ namespace RockPaperScissors
 {
     public partial class Form1 : Form
     {
-        private readonly string[] outcomes = { "Камень", "Ножницы", "Бумага" };
-        private string GetComputerOutcome()
-        {
-            Random rand = new Random();
-            return outcomes[rand.Next(0, 3)];
-        }
-        public string UserOutcome { get; private set; }
-        public string ComputerOutcome { get; private set; }
-        public int Wins { get; private set; }
-        public int Loses { get; private set; }
-        public int Draws { get; private set; }
         public Form1()
         {
             InitializeComponent();
             labelCompOutcome.Hide();
             labelGameResult.Hide();
         }
+        private string GetComputerOutcome()
+        {
+            return outcomes[rand.Next(0, 3)];
+        }
+        private readonly string[] outcomes = { "Камень", "Ножницы", "Бумага" };
+        
+        private string userOutcome;
+        private string computerOutcome;
+        private int wins;
+        private int loses;
+        private int draws;
+
+        private Random rand = new Random();
 
         private void bRock_Click(object sender, EventArgs e)
         {
-            UserOutcome = outcomes[0];
-            ComputerOutcome = GetComputerOutcome();
-            labelCompOutcome.Text = $"Компьютер выбрал:\n{ComputerOutcome}";
+            userOutcome = outcomes[0];
+            computerOutcome = GetComputerOutcome();
+            labelCompOutcome.Text = $"Компьютер выбрал:\n{computerOutcome}";
             labelCompOutcome.Show();
-            if (ComputerOutcome == outcomes[0]) { Draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
-            if (ComputerOutcome == outcomes[1]) { Wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
-            if (ComputerOutcome == outcomes[2]) { Loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
+            if (computerOutcome == outcomes[0]) { draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
+            if (computerOutcome == outcomes[1]) { wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
+            if (computerOutcome == outcomes[2]) { loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
             labelGameResult.Show();
-            labelStats.Text = $"Побед: {Wins} Поражений: {Loses} Ничьих: {Draws}";
+            labelStats.Text = $"Побед: {wins} Поражений: {loses} Ничьих: {draws}";
         }
 
         private void bScissors_Click(object sender, EventArgs e)
         {
-            UserOutcome = outcomes[1];
-            ComputerOutcome = GetComputerOutcome();
-            labelCompOutcome.Text = $"Компьютер выбрал:\n{ComputerOutcome}";
+            userOutcome = outcomes[1];
+            computerOutcome = GetComputerOutcome();
+            labelCompOutcome.Text = $"Компьютер выбрал:\n{computerOutcome}";
             labelCompOutcome.Show();
-            if (ComputerOutcome == outcomes[1]) { Draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
-            if (ComputerOutcome == outcomes[2]) { Wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
-            if (ComputerOutcome == outcomes[0]) { Loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
+            if (computerOutcome == outcomes[1]) { draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
+            if (computerOutcome == outcomes[2]) { wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
+            if (computerOutcome == outcomes[0]) { loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
             labelGameResult.Show();
-            labelStats.Text = $"Побед: {Wins} Поражений: {Loses} Ничьих: {Draws}";
+            labelStats.Text = $"Побед: {wins} Поражений: {loses} Ничьих: {draws}";
         }
 
         private void bPaper_Click(object sender, EventArgs e)
         {
-            UserOutcome = outcomes[2];
-            ComputerOutcome = GetComputerOutcome();
-            labelCompOutcome.Text = $"Компьютер выбрал:\n{ComputerOutcome}";
+            userOutcome = outcomes[2];
+            computerOutcome = GetComputerOutcome();
+            labelCompOutcome.Text = $"Компьютер выбрал:\n{computerOutcome}";
             labelCompOutcome.Show();
-            if (ComputerOutcome == outcomes[2]) { Draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
-            if (ComputerOutcome == outcomes[0]) { Wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
-            if (ComputerOutcome == outcomes[1]) { Loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
+            if (computerOutcome == outcomes[2]) { draws++; labelGameResult.Text = "Ничья!"; labelGameResult.ForeColor = Color.Black; }
+            if (computerOutcome == outcomes[0]) { wins++; labelGameResult.Text = "Вы победили!"; labelGameResult.ForeColor = Color.Green; }
+            if (computerOutcome == outcomes[1]) { loses++; labelGameResult.Text = "Вы проиграли!"; labelGameResult.ForeColor = Color.Red; }
             labelGameResult.Show();
-            labelStats.Text = $"Побед: {Wins} Поражений: {Loses} Ничьих: {Draws}";
+            labelStats.Text = $"Побед: {wins} Поражений: {loses} Ничьих: {draws}";
+        }
+
+        private void bExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
